29e46e723311e7afd2a82572e47d18ecb1124cc5
Maciej Fuhrmann <fuhrmann.maciej@gmail.com>
2026-05-05 13:21:16 +0200

    added config properties and config reader to store variables in one place

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,18 +1,19 @@
 package base;
 
+import core.config.ConfigReader;
 import core.driver.DriverFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Optional;
-import org.testng.annotations.Parameters;
 
 public class BaseTest {
 
     @BeforeMethod
-    @Parameters("browser")
-    public void setUp(@Optional("chrome") String browser) {
+    public void setUp() {
+        String browser = ConfigReader.get("browser");
+
         DriverFactory.initDriver(browser);
         DriverFactory.getDriver().manage().window().maximize();
+
     }
 
     @AfterMethod
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import base.BaseTest;
+import core.config.ConfigReader;
 import core.driver.DriverFactory;
 import org.testng.annotations.Test;
 import pages.LoginPage;
@@ -10,14 +11,15 @@ public class LoginTest extends BaseTest {
     @Test
     public void shouldLoginSuccessfully() {
 
+        String url = ConfigReader.get("base.url");
+
         LoginPage loginPage = new LoginPage(DriverFactory.getDriver());
 
         loginPage
-                .open("https://example.com")
+                .open(url)
                 .enterUsername("user")
                 .enterPassword("password")
                 .clickLogin();
 
-        // Assertion here
     }
 }
